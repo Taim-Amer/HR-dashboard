6a8b7bd185b8e76fad367201428963d60c180d38
Taim-Amer <taim.amer.000@gmail.com>
2024-12-16 01:44:47 -0800

    Post request

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
+const databaseLink = 'mongodb+srv://devtaim:PNFGRSsHGVS00dIr@cluster0.1vbrm.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0';
+app.use(express.urlencoded({extended:true}));
+const MyData = require("./models/schema");
+
 
 app.get('/', (req, res) => {
-    res.sendFile("./views/home.html", { root: __dirname })
+    res.sendFile("./views/home.html", {root: __dirname})
 })
 
-// app.listen(port, () => {
-//     console.log(``)
-// })
-
-mongoose.connect('mongodb+srv://devtaim:PNFGRSsHGVS00dIr@cluster0.1vbrm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => { 
+mongoose.connect(databaseLink).then(() => {
     app.listen(port, () => {
-        console.log('http://localhost:${port}/')
+        console.log(`http://localhost:${port}/`);
     })
 }).catch((error) => {
     console.log(error);
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.redirect("/index.html")
 })
---
+++ models/schema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Define the Schema (Structure)
+const articleSchema = new Schema({
+    username: String,
+});
+
+//Create a Model based on that Schema
+const MyData = mongoose.model("MyDataa", articleSchema);
+
+//Export the Model
+module.exports = MyData;
+
+
+
