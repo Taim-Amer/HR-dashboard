d43cc2f37daadf5e40597166d95efc1af5e17ba8
Taim-Amer <taim.amer.000@gmail.com>
2024-12-17 01:04:07 -0800

    Project Bootstrap design

--- app.js
+++ app.js
@@ -3,37 +3,30 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 const databaseLink = 'mongodb+srv://devtaim:PNFGRSsHGVS00dIr@cluster0.1vbrm.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0';
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 const MyData = require("./models/schema");
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-//Auto Refresh
-const path = require("path");
-const livereload = require("livereload");
-const liveReloadServer = livereload.createServer({ port: 35731 });
-liveReloadServer.watch(path.join(__dirname, 'public'));
-
-
-const connectLivereload = require("connect-livereload");
-app.use(connectLivereload());
-
-liveReloadServer.server.once("connection", () => {
-    setTimeout(() => {
-        liveReloadServer.refresh("/");
-    }, 100);
-});
-
 app.get('/', (req, res) => {
-    MyData.find().then((result) => {
-        res.render('home', {myTitle : "Home Page", arr : result});
-    }).catch((error) => {
-        console.log(error)
-    })
+    res.render('index', {});
+
 })
 
-app.get('/index.html', (req, res) => {
-    res.send("<h1> Send Successfully !</h1>");
+app.get('/user/add.html', (req, res) => {
+    res.render("user/add");
+})
+
+app.get('/user/edit.html', (req, res) => {
+    res.render('user/edit');
+})
+
+app.get('/user/search.html', (req, res) => {
+    res.render('user/search'); 
+})
+
+app.get('/user/view.html', (req, res) => {
+    res.render('user/view')
 })
 
 mongoose.connect(databaseLink).then(() => {
